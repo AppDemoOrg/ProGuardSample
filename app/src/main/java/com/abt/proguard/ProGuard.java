317a6b4ca61acd32277102a2fb76efbe1bb4a3b2
huangweiqi <askviky2010@gmail.com>
2018-06-10 18:37:26 +0800

    添加样例代码

---
+++ app/src/main/java/com/abt/proguard/ProGuard.java
@@ -0,0 +1,21 @@
+package com.abt.proguard;
+
+/**
+ * @描述： @ProGuard
+ * @作者： @黄卫旗
+ * @创建时间： @10/06/2018
+ */
+public class ProGuard {
+
+    private int id;
+    private int hash;
+
+    public class Helper {
+        private String name;
+        private String skill;
+    }
+
+    public void guard(String what) {
+        System.out.println("just do it!!!");
+    }
+}
---
+++ app/src/main/java/com/abt/proguard/bean/Guard.java
@@ -0,0 +1,13 @@
+package com.abt.proguard.bean;
+
+/**
+ * @描述： @Guard
+ * @作者： @黄卫旗
+ * @创建时间： @10/06/2018
+ */
+public class Guard {
+
+    private String name;
+    private String phone;
+
+}
